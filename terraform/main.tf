c4fc6169fb0f6449491c501b967ae6cb73e82497
SubramanyamGit <subramanyamsiliveri@gmail.com>
2024-11-13 12:32:44 +0000

    removed duplicated

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,18 +82,18 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 # Network Interface
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+#resource "azurerm_network_interface" "nic" {
+ # name                = "${var.vm_name}-nic"
+  #location            = azurerm_resource_group.rg.location
+  #resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
+  #ip_configuration {
+   # name                          = "internal"
+    #subnet_id                     = azurerm_subnet.subnet.id
+    #private_ip_address_allocation = "Dynamic"
+    #public_ip_address_id          = azurerm_public_ip.public_ip.id
+ # }
+#}
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "Network-NSG"
